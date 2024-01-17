711c05059a253489d9954717ac663015a90731ef
Mohammad-Almomani <mohammad.momani970@gmail.com>
2024-01-17 14:40:41 +0300

    enhance performance

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import Aos from "aos";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import AppRoutes from "./components/Routes";
+// import "./css/bootstrap-icons.css";
+// import "./css/bootstrap.min.css";
 
 function App() {
+  const [isCssLoaded, setCssLoaded] = useState(false);
   useEffect(() => {
-    Aos.init({ offset: 100, duration: 750, easing: 'ease-in-sine', delay: 10});
+    import('./css/safe-zone-template.css')
+      .then(() => {
+        // Set state to indicate that the CSS file has been loaded
+        setCssLoaded(true);
+        Aos.init({ offset: 100, duration: 750, easing: 'ease-in-sine', delay: 10});
+      })
   }, [])
-
+  
+  // import "./css/safe-zone-template.css"
   return (
-    <div>
-        <AppRoutes />
+    <div id="app" style={{ display: isCssLoaded ? 'block' : 'none' }}>
+              <AppRoutes />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,9 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.css";
-import "./css/bootstrap-icons.css";
-import "./css/bootstrap.min.css";
-import "./css/safe-zone-template.css";
 import 'aos/dist/aos.css';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
