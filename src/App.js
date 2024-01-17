0d02408a02508f700f070d6454ee4f8882b3636c
Mohammad-Almomani <mohammad.momani970@gmail.com>
2024-01-17 14:50:46 +0300

    fixed issue

--- src/App.js
+++ src/App.js
@@ -7,15 +7,13 @@ import AppRoutes from "./components/Routes";
 function App() {
   const [isCssLoaded, setCssLoaded] = useState(false);
   useEffect(() => {
+    Aos.init({ offset: 100, duration: 750, easing: 'ease-in-sine', delay: 10});
     import('./css/safe-zone-template.css')
       .then(() => {
-        // Set state to indicate that the CSS file has been loaded
         setCssLoaded(true);
-        Aos.init({ offset: 100, duration: 750, easing: 'ease-in-sine', delay: 10});
       })
   }, [])
   
-  // import "./css/safe-zone-template.css"
   return (
     <div id="app" style={{ display: isCssLoaded ? 'block' : 'none' }}>
               <AppRoutes />
